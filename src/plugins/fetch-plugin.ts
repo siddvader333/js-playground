92493555886c2638104cd88dc834f41805844e6d
Siddharth Vijay <siddharth.vijay333@gmail.com>
2021-06-10 15:41:27 -0400

    split basic plugin into path solver plugin and file fetching plugin

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,42 @@
+/*Use this plugin to handle file fetching (e.g. from npm) */
+import axios from "axios";
+import * as esbuild from "esbuild-wasm";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "fileCache",
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: "fetch-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: inputCode,
+          };
+        }
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+    },
+  };
+};
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,51 +1,25 @@
-import axios from "axios";
 import * as esbuild from "esbuild-wasm";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
 
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
+      build.onResolve({ filter: /^\.+\// }, async (args: any) => {
         return {
           namespace: "a",
-          path: `https://unpkg.com/${args.path}`,
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: `
-              const message = require('react');
-              const reactDOM = require('react-dom')
-              console.log(message);
-              console.log(reactDOM);
-            `,
-          };
-        }
-        const { data, request } = await axios.get(args.path);
-        console.log(data);
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname,
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
         };
       });
     },
