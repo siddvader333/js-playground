b66bba8583bd05140de6f65d42febd63f263694e
Siddharth Vijay <siddharth.vijay333@gmail.com>
2021-06-11 11:30:13 -0400

    Added onlistener on iframe to make sure code does not get cut off + error message display + iframe refresh before bundle

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState({
     "message.js": 'module.exports = "Hello World!"',
     "index.js": "",
   });
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -27,6 +27,8 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+    iframe.current.srcdoc = html;
+
     //console.log(ref.current);
     const result = await ref.current.build({
       entryPoints: ["index.js"],
@@ -38,15 +40,30 @@ const App = () => {
         global: "window",
       },
     });
-    /*const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
-    });*/
 
-    //console.log(result.);
-    setCode(result.outputFiles[0].text);
-    eval(result.outputFiles[0].text);
+    //setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root">
+          <script> 
+            window.addEventListener('message', (event) => {
+              console.log(event);
+              try{
+                eval(event.data);
+              }catch(err){
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err+ '</div>';
+              }
+            }, false)
+          </script>
+        </div>
+      </body>
+    </html>`;
   return (
     <div>
       <textarea
@@ -58,7 +75,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="display"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      ></iframe>
     </div>
   );
 };
