94a5f91c863bde697aa3eee297b083c56809ac11
Siddharth Vijay <siddharth.vijay333@gmail.com>
2021-06-18 18:43:55 -0400

    react-particle-js integration

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import CellList from "./components/CellList/CellList";
 import { store } from "./redux";
 import Navbar from "./components/Navbar/Navbar";
+import Particles from "react-particles-js";
 
 const App = () => {
   return (
@@ -14,6 +15,35 @@ const App = () => {
       <div>
         <CellList />
       </div>
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 50,
+            },
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5,
+              },
+            },
+            size: {
+              value: 3,
+            },
+            color: { value: "#000000" },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+        }}
+      />
     </Provider>
   );
 };
